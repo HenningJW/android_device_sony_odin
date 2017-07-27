0001dc39a8683319c0191281c3c212a2d13e9928
HenningJW <HenningJW@users.noreply.github.com>
2017-07-27 16:20:52 +0200

    [Do not merge, personal] audio: increase volume steps
    For finer volume adjustment.

--- product/audio.mk
+++ product/audio.mk
@@ -2,3 +2,8 @@
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_policy_configuration.xml:system/etc/audio_policy_configuration.xml \
     $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml
+
+# Volume Steps
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.config.vc_call_vol_steps=14 \
+    ro.config.media_vol_steps=50
